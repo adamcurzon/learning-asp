7d27ae31ba1d1be19d679022f1219219213b3da6
acurzon <acurzon@serif.com>
2023-07-08 13:54:22 +0100

    refactor: remove default controllers and add CarController

---
+++ Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learning_asp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CarController : ControllerBase
+	{
+		public CarController()
+		{
+		}
+
+		[HttpGet ]
+		public string GetCarName() {
+			return "Ford Fiesta";
+		}
+	}
+}
+
